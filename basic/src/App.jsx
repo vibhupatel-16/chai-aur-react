89713774f710a2eb83f845aacea128827af5642d
vibhupatel-16 <vibhupatel1602@gmail.com>
2025-09-04 11:29:25 +0530

    initial commit

---
+++ basic/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Chai from "./chai";
+function App(){
+  return(
+    <>
+    <h1>React Basic</h1>
+    <Chai/>
+    </>
+  )
+}
+export default App;
---
+++ basic/src/chai.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+function Chai(){
+    return(
+        <h2>Chai or Code</h2>
+    )
+}
+export default Chai
